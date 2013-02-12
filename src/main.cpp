91a21c4837ce7c9f84157306c87bfd7627d92233
Tiago Andrade <tiago.andrade@unep-wcmc.org>
2013-02-12 15:36:28 +0000

    Determines image classes and files for each class

--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,45 @@
 #include <iostream>
 #include <iterator>
 #include <algorithm>
+#include <string>
+using namespace std;
 
 #include <opencv2/opencv.hpp>
-using namespace cv;
 
-#include <boost/lambda/lambda.hpp>
-using namespace boost::lambda;
+#include <boost/filesystem.hpp>
+using namespace boost::filesystem;
 
-int main(int argc, char** argv)
-{
-	// Test OpenCV
-	Mat image = imread("data/demo.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+typedef vector<path> pthvec;
 
-	namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
-	imshow("Display Image", image);
 
-	waitKey(0);
+// Returns the list of the relative paths of every file inside a folder.
+pthvec list_files(string directory) {
+	path dir(directory);
 
-	// Test Boost
-	typedef std::istream_iterator<int> in;
+	pthvec filenames;
+	for(directory_iterator it(dir); it != directory_iterator(); ++it) {
+		filenames.push_back(it->path().relative_path());
+	}
+	
+	return filenames;
+}
 
-	std::for_each(
-		in(std::cin), in(), std::cout << (_1 * 3) << " " );
+int main(int argc, char** argv) {
+	pthvec classes = list_files("data/scene_categories");
+	for(pthvec::iterator it = classes.begin(); it != classes.end(); ++it) {
+		cout << "Class: " << it->filename().string() << endl;
+		pthvec filenames = list_files(it->string());
+		for(pthvec::iterator it2 = filenames.begin(); it2 != filenames.end(); ++it2) {
+			cout << it2->filename().string() << ",";
+			cv::Mat image = cv::imread(it->string(), CV_LOAD_IMAGE_GRAYSCALE);
+			
+			//cv::namedWindow(*it, cv::CV_WINDOW_AUTOSIZE);
+			//cv::imshow(*it, image);
+		}
+		cout << endl;
+	}
 
+	cv::waitKey(0);
 
 	return 0;
 }
