5eec0e92a9df98b5ce542b1c80ced6dfd482f61a
Tiago Andrade <tiago.andrade@unep-wcmc.org>
2013-02-13 16:59:49 +0000

    Parallelized feature computation and added vocabulary serialization

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@ using namespace std;
 
 #include <boost/foreach.hpp>
 #include <boost/filesystem.hpp>
-
 #include <boost/assign/list_of.hpp>
 using namespace boost::filesystem;
 
@@ -26,7 +25,6 @@ const cv::Ptr<cv::DescriptorMatcher> descriptor_matcher =
 
 // Returns the list of the relative paths of every file inside a folder.
 pthvec list_files(string directory, bool recursive=false) {
-	
 	path dir(directory);
 	pthvec filenames;
 		
@@ -48,14 +46,19 @@ pthvec list_files(string directory, bool recursive=false) {
 
 
 // Computes the codebook using the bag-of-words technique 
-cv::Mat compute_codebook(const string dataset) {
+cv::Mat compute_codebook(const string dataset,
+	const unsigned int codebook_size) {
 	// Initialize Cluster Trainer
-	cv::BOWKMeansTrainer codebook_trainer(100);
+	cv::BOWKMeansTrainer codebook_trainer(codebook_size);
 
 
 	// Go trough all classes in the given dataset
 	pthvec filenames = list_files(dataset, true);
-	BOOST_FOREACH(path img, filenames) {
+	
+	#pragma omp parallel for
+	for(unsigned int i = 0 ; i < filenames.size(); i++) {
+		path img = filenames[i];
+		
 		// Load the image
 		cout << img.filename().string() << endl;
 		cv::Mat input = cv::imread(img.string(), CV_LOAD_IMAGE_GRAYSCALE);
@@ -68,47 +71,51 @@ cv::Mat compute_codebook(const string dataset) {
 		descriptor_extractor->compute(input, keypoints, features);
 		
 		// Train bag-of-words
-	    codebook_trainer.add(features);
+		#pragma omp critical 
+		{
+	    	codebook_trainer.add(features);
+	    }
 	}
 	
 	// Train the clusters
 	cv::Mat vocabulary = codebook_trainer.cluster();
 	//TODO Store vocabulary
 	
+	cv::FileStorage fs("vocab.xml", cv::FileStorage::WRITE);
+	fs << "vocabulary" << vocabulary;
+	
 	return vocabulary;
 }
 
 void train_classifier(const string dataset,
-	const int num_training_imgs, const cv::Mat& vocabulary) {
+	const unsigned int num_training_imgs, const cv::Mat& vocabulary) {
 	// Get new descriptors for images
 	cv::BOWImgDescriptorExtractor bow_extractor(
 		descriptor_extractor, descriptor_matcher);
 		
 	bow_extractor.setVocabulary(vocabulary);
 	
-	//TODO Init matrices
 	cv::Mat samples;
 	cv::Mat labels;
 	
-	int class_idx = 0;
+	unsigned int class_idx = 0;
 
 	// Go trough all classes in the given dataset
 	pthvec classes = list_files(dataset);
 	BOOST_FOREACH(path cls, classes) {
 		cout << "Class: " << cls.filename().string() << endl;
 		
-		int image_idx = 0;
 		// Go trough each picture in the class
 		pthvec filenames = list_files(cls.string());
 		shuffle(filenames.begin(), filenames.end(),
 			std::default_random_engine(0));
-		BOOST_FOREACH(path img, filenames) {
-			if(image_idx >= num_training_imgs) {
-				break;
-			}
+		
+		#pragma omp parallel for
+		for(unsigned int i = 0;
+			i < min(num_training_imgs, filenames.size()); i++) {
+			path img = filenames[i];
 		
 			// Load the image
-			cout << img.filename().string() << endl;
 			cv::Mat input = cv::imread(img.string(), CV_LOAD_IMAGE_GRAYSCALE);
 			
 			// Extract features
@@ -118,15 +125,19 @@ void train_classifier(const string dataset,
 			// Compute image final descriptor
 			cv::Mat img_descriptor;
 		    bow_extractor.compute(input, keypoints, img_descriptor);
-		    cout << img_descriptor << endl;
 		    
-		    samples.push_back(img_descriptor);
-		    labels.push_back(class_idx);
-		    
-		    image_idx++;
+		    # pragma omp critical
+		    {
+		    	cout << img.filename().string() << endl;
+				cout << img_descriptor << endl;
+				samples.push_back(img_descriptor);
+				labels.push_back(class_idx);
+			}
 		}
 		class_idx++;
 	}
+	
+	cout << "Labels: " << labels << endl;
 	
 	// Train the classifier
 	cout << "Training started" << endl;
@@ -135,9 +146,21 @@ void train_classifier(const string dataset,
 }
 
 int main(int argc, char** argv) {
+	const unsigned int codebook_size = 100;
+	const unsigned int num_training_images = 100;
 	string dataset = "data/simple_scene_categories";
-	cv::Mat vocabulary = compute_codebook(dataset);
-	train_classifier(dataset, 100, vocabulary);
+	
+	cv::Mat vocabulary;
+	
+	if(exists("vocab.xml")) {
+		cv::FileStorage fs("vocab.xml", cv::FileStorage::READ);
+		fs["vocabulary"] >> vocabulary;
+	} else {
+		vocabulary = compute_codebook(dataset, codebook_size);
+	}
+	
+	
+	train_classifier(dataset, num_training_images, vocabulary);
 	//test_classifier
 
 	return 0;
