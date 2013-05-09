864d1fc40115bdc0bb507b623c582ffe0f77e7fd
Tiago Andrade <tiago.andrade@unep-wcmc.org>
2013-05-09 13:55:17 +0100

    Added bias to Linear classifier

--- src/classification/LinearClassifier.cpp
+++ src/classification/LinearClassifier.cpp
@@ -58,12 +58,14 @@ void LinearClassifier::train(vector<Histogram*> histograms,
 	unsigned int currentIter = 0;
 	#pragma omp parallel for
 	for(unsigned int i = 0; i < histograms.size(); i++) {
-		kernel[i] = new linear::feature_node[descriptorLength + 1];
+		kernel[i] = new linear::feature_node[descriptorLength + 2];
 		for(unsigned int j = 0; j < descriptorLength; j++) {				
 			kernel[i][j].index = j + 1;
 			kernel[i][j].value = histograms[i]->getData()[j];
 		}
-		kernel[i][descriptorLength].index = -1;
+		kernel[i][descriptorLength].index = descriptorLength + 1;
+		kernel[i][descriptorLength].value = 1.0;
+		kernel[i][descriptorLength + 1].index = -1;
 		
 		#pragma omp critical
 		{
@@ -76,10 +78,10 @@ void LinearClassifier::train(vector<Histogram*> histograms,
 	vector<int> newClasses(imageClasses.begin(), imageClasses.end());
 	m_svmProb = new linear::problem();
 	m_svmProb->l = histograms.size();
-	m_svmProb->n = descriptorLength;
+	m_svmProb->n = descriptorLength + 1;
 	m_svmProb->y = &newClasses[0];
 	m_svmProb->x = kernel;
-	m_svmProb->bias = 0;
+	m_svmProb->bias = 1;
 	
 	m_svmModel = linear::train(m_svmProb, m_svmParams);
 	cout << endl;
@@ -87,14 +89,16 @@ void LinearClassifier::train(vector<Histogram*> histograms,
 
 pair<unsigned int, double> LinearClassifier::classify(Histogram* histogram) {
 	const unsigned int histLength = histogram->getLength();
-	linear::feature_node testNode[histLength + 1];
+	linear::feature_node testNode[histLength + 2];
 
 	#pragma omp parallel for
 	for(unsigned int j = 0; j < histLength; j++) {				
 		testNode[j].index = j + 1;
 		testNode[j].value = histogram->getData()[j];
 	}
-	testNode[histLength].index = -1;
+	testNode[histLength].index = histLength + 1;
+	testNode[histLength].value = 1.0;
+	testNode[histLength + 1].index = -1;
 	
 	vector<double> probabilities(m_classNames.size(), 0.0);
 	unsigned int predictedClass =
