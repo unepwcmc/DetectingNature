b29108ca92aa37af9bf34ef4d597300261335e92
Tiago Andrade <tiago.andrade@unep-wcmc.org>
2013-07-01 14:22:23 +0100

    Added missing dependencies

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
+cd libs/gmm-fisher
+cmake .
+make
+
+cd ../vlfeat-0.9.16
+make
+
+cd ../yael_v300
+./configure.sh
+make
+
+cd ../..
 mkdir -p build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=.. ../src
